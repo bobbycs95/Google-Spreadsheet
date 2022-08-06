0c61f12d1d3d556625be818d8712c7fba11925bc
bobbycs95 <104830394+bobbycs95@users.noreply.github.com>
2022-08-06 14:07:52 +0700

    ATM_Action
    This project is to demonstrate the use of Class in Python. I will use the example of ATM method (login, check, deposit, and withdraw) to demonstrate the usage of Class.

---
+++ atm_action_class.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 14:02:08 2022
+"""
+
+class State:
+  def __init__(self,password,balance = 0):
+    self.password = password
+    self.balance = balance
+    if self.password == "hacker":
+      c = "Authorized"
+      s = "True"
+    else:
+      c = "Unauthorized"
+      s = "False"
+    self.cust = str
+    self.states = str
+    self.s = s
+    self.c = c 
+
+  def login(self):
+    print(f"Success = {self.s} {self.c}")
+
+  def check(self):
+    print(f"Success = {self.s} {self.c} {self.balance}")
+
+  def deposit(self,amount):
+    if self.password == "hacker":
+      self.balance += amount
+    print(f"Success = {self.s} {self.c} {self.balance}")
+    
+  def withdraw(self,amount):
+    if self.password == "hacker":
+      self.balance -= amount
+      if self.balance < 0 :
+        self.s = "False"
+        self.balance += amount
+    print(f"Success = {self.s} {self.c} {self.balance}")
+
+px = State("hacker",0)
+px.login()
+px.deposit(50)
+px.withdraw(35)
+px.deposit(47)
+px.withdraw(63)
